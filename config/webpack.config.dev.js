7786a4bf917c50550ed47f3c8d96333e2ffee5fa
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2020-03-03 10:50:53 +0000

    fix: cors headers to allow access to assets from editor-demo

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -49,5 +49,10 @@ module.exports = {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+    },
   },
 };
