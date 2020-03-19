ac626a9d793cf4188b1301e64bf7fc1fcaf406cf
NuclearRedeye <52923935+NuclearRedeye@users.noreply.github.com>
2020-03-19 15:17:29 +0000

    fix: simplify solution

--- src/article/converter/jats/ParagraphConverter.js
+++ src/article/converter/jats/ParagraphConverter.js
@@ -11,20 +11,12 @@ export default class ParagraphConverter {
   }
 
   import(el, node, importer) {
-    // NOTE: The intent of this is to capture and handle different formatting of XML...
-    for (const child of el.childNodes) {
-      if (child.el && child.el instanceof Text) {
-        // If the line contains a LF or CR
-        // prettier-ignore
-        const reContainsLfOrCR = new RegExp('.*[\n\l].*');
-        if (reContainsLfOrCR.test(child.el.nodeValue)) {
-          // And only contains spaces...
-          const reOnlyLrCrOrSpace = new RegExp('^[\n\r ]*$');
-          if (reOnlyLrCrOrSpace.test(child.el.nodeValue)) {
-            // Then remove it...
-            el.removeChild(child);
-          }
-        }
+    // If the paragraph starts with a LF or CR and then only contains spaces, then ignore it.
+    const child = el.firstChild;
+    if (child && child.el && child.el instanceof Text) {
+      const reOnlyLrCrOrSpace = new RegExp('^(\n|\r) +$');
+      if (reOnlyLrCrOrSpace.test(child.el.nodeValue)) {
+        el.removeChild(child);
       }
     }
     node.content = importer.annotatedText(el, [node.id, 'content']);
