14dd546a61faa4517a49ee67bd8676acdcf55a00
NuclearRedeye <52923935+NuclearRedeye@users.noreply.github.com>
2020-03-19 14:02:50 +0000

    fix: kludge to fix extra space in bio

--- src/article/converter/jats/ParagraphConverter.js
+++ src/article/converter/jats/ParagraphConverter.js
@@ -11,6 +11,22 @@ export default class ParagraphConverter {
   }
 
   import(el, node, importer) {
+    // NOTE: The intent of this is to capture and handle different formatting of XML...
+    for (const child of el.childNodes) {
+      if (child.el && child.el instanceof Text) {
+        // If the line contains a LF or CR
+        // prettier-ignore
+        const reContainsLfOrCR = new RegExp('.*[\n\l].*');
+        if (reContainsLfOrCR.test(child.el.nodeValue)) {
+          // And only contains spaces...
+          const reOnlyLrCrOrSpace = new RegExp('^[\n\r ]*$');
+          if (reOnlyLrCrOrSpace.test(child.el.nodeValue)) {
+            // Then remove it...
+            el.removeChild(child);
+          }
+        }
+      }
+    }
     node.content = importer.annotatedText(el, [node.id, 'content']);
   }
 
