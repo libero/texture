7bc21e66ab75bd4528b9e93adcac96f1ed5cb44d
Joel Summerfield <52923935+NuclearRedeye@users.noreply.github.com>
2020-03-19 15:50:14 +0000

    fix: extra space at start of author bio (#40)
    Added some logic to the paragraph importer to ignore lines that start with either a LF or CR and only contain spaces.

--- src/article/converter/jats/ParagraphConverter.js
+++ src/article/converter/jats/ParagraphConverter.js
@@ -11,6 +11,14 @@ export default class ParagraphConverter {
   }
 
   import(el, node, importer) {
+    // If the paragraph starts with a LF or CR and then only contains spaces, then ignore it.
+    const child = el.firstChild;
+    if (child && child.el && child.el instanceof Text) {
+      const reOnlyLrCrOrSpace = new RegExp('^(\n|\r) +$');
+      if (reOnlyLrCrOrSpace.test(child.el.nodeValue)) {
+        el.removeChild(child);
+      }
+    }
     node.content = importer.annotatedText(el, [node.id, 'content']);
   }
 
